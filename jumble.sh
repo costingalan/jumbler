4631d0f7cd410a4a5efd1f22793116eaa0ffcb4c
Alex Mitan <alex.mitan@yahoo.com>
2016-02-06 18:02:21 +0200

    Initial commit

---
+++ jumble.sh
@@ -0,0 +1,33 @@
+pic=$1
+dest=$2
+echo "Jumbling initiated"
+if [ -f "$pic" ]
+then
+    echo "Attempting to jumble $pic into $dest"
+    
+    split $pic -n 3 ${pic} # original -> a b c -> a c b
+                       # jumbled  -> a c b -> a b c
+    echo "Split into cha[a-c]"
+    
+
+    
+    if [ -n "$dest" ]   # if a destination is provided write there
+    then
+        if [ -f "$dest" ]
+        then
+            rm $dest
+            echo "File $dest overwritten"
+            touch $dest
+        fi
+        cat ${pic}aa ${pic}ac ${pic}ab > $dest
+        echo "$pic jumbled into $dest"
+    else                # otherwise overwrite the file
+        cat ${pic}aa ${pic}ac ${pic}ab > $pic
+        echo "$pic (re?)jumbled"
+    fi
+    
+    rm ${pic}aa ${pic}ab ${pic}ac   # cleanup
+else
+    echo "Filename absent or invalid!"
+fi
+
