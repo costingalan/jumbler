167dd65e1929874ed390bf7805550a742c616ef7
Costin Galan <cgalan@cloudbasesolutions.com>
2016-02-06 18:30:33 +0200

    TO-DO: Add command line option for number of chunks
    Signed-off-by: Costin Galan <cgalan@cloudbasesolutions.com>

--- jumble.sh
+++ jumble.sh
@@ -5,12 +5,21 @@
 # Description: This script will take an image, split it in chunks and then recode it 
 
 if [[ $# -lt 2 ]]; then
-  echo "Usage: ./$0 source_picture destination_picture"
-  return 1
+    echo "Usage: ./$0 source_picture destination_picture"
+    return 1
 fi
 
 pic=$1
 dest=$2
+
+#TO-DO: Command line parameter for number of chunks
+
+#Check if $3 or $chunks variable is set
+#chunks=$3 
+#if [[ -z ${chunks+x} ]]; then
+#    chunks=3 
+#fi
+
 echo "Jumbling initiated"
 if [[ -f "$pic" ]]; then
     echo "Attempting to jumble $pic into $dest"
@@ -18,8 +27,6 @@ if [[ -f "$pic" ]]; then
     split "$pic" -n 3 "${pic}" # original -> a b c -> a c b
                        # jumbled  -> a c b -> a b c
     echo "Split into cha[a-c]"
-    
-
     
     if [[ -n "$dest" ]]; then   # if a destination is provided write there
         if [[ -f "$dest" ]]; then
